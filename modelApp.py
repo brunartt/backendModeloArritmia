5b9b2ad8e118d80819d7baec78968564c34818b0
Bruna Roberta <144182969+brunartt@users.noreply.github.com>
2025-07-05 14:57:13 -0300

    criarção do endpoint POST para predição de arritmia

--- modelApp.py
+++ modelApp.py
@@ -1,13 +1,9 @@
 from flask import Flask, request, jsonify
-import numpy as np
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.ensemble import RandomForestClassifier
 import joblib
 
 app = Flask(__name__)
 
-# Carrega modelo e scaler
 model = joblib.load('models/meu_modelo_de_arritmia.pkl')
 scaler = joblib.load('models/escalador.pkl')
 
@@ -18,22 +14,20 @@ def home():
 @app.route('/prever', methods=['POST'])
 def prever():
     try:
-        # Dados JSON do corpo da requisição
         dados = request.json
         if not dados:
             return jsonify({'erro': 'Nenhum dado recebido'}), 400
 
-        # Converte para DataFrame
+        # Converte os dados recebidos para DataFrame
         df = pd.DataFrame([dados])
 
-        # Aplica escalonamento
+        # Aplica o escalonamento
         dados_escalonados = scaler.transform(df)
 
         # Faz predição
         predicao = model.predict(dados_escalonados)
 
-        # Retorna resultado
-        return jsonify({'predicao': int(predicao[0])})
+        return jsonify({'predicao': str(predicao[0])})
 
     except Exception as e:
         return jsonify({'erro': str(e)}), 500
